b5d5acb4ba53c55a6b68df0a2ed4495b19887cbf
K-ai30 <artpirate11@aim.com>
2020-04-04 18:27:22 -0400

    changed schema model

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -3,9 +3,39 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-    description: {type: String, required: true },
-    duration: { type: Number, required: true },
-    date: { type: Date, required: true },
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise type"
+        },
+        name: {
+            type: String,
+            trim: true, 
+            required: "Enter an exercise name"
+        },
+        duration: { 
+            type: Number,
+            trim: true, 
+            required: "Enter an exercise duration in minutes"
+        },
+        weight: { 
+            type: Number
+        },
+        reps: { 
+            type: Number
+        },
+        sets: { 
+            type: Number
+        },
+        distance: { 
+            type: Number
+        },
+    }]
 }, {
     timestamps: true,
 });
